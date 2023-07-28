aa59fe6d0a7892ac78a043a9a603d9ea124a13e9
willmreed14 <wreed2020@fau.edu>
2023-07-27 22:36:53 -0400

    Part 12 - auth

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 /* WHERE ALL JS CODE GOES */
 // Import firebase functions that we need
+
+// imports from app
 import { initializeApp } from 'firebase/app'
+
+// imports from firestore
 import { 
     getFirestore, collection, getDocs, onSnapshot,
     addDoc, deleteDoc, doc, 
-    query, where
+    query, where,
+    orderBy, serverTimestamp,
+    getDoc, updateDoc
 } from 'firebase/firestore'
+
+// imports from auth
+import {
+    getAuth, createUserWithEmailAndPassword
+} from 'firebase/auth'
 
 const firebaseConfig = { // which project are we connecting to?
     apiKey: "AIzaSyBQlSup34Crk1cSQITesL_eXNwsdyNPTXQ",
@@ -21,12 +32,13 @@ initializeApp(firebaseConfig)
 
 // init services
 const db = getFirestore()
+const auth = getAuth()
 
 // collection ref
 const colRef = collection(db, 'books')
 
 // queries
-const q = query(colRef, where("author", "==", "subtronics")) // fetch docs in col. WHERE x = y 
+const q = query(colRef, orderBy('createdAt')) // fetch docs in col. WHERE x = y 
 
 // ** REAL TIME ** GET (fetch) the documents in a collection (query now tho)
 onSnapshot(q, (snapshot) =>{ // fire function whenever a change occurs
@@ -60,6 +72,7 @@ addBookForm.addEventListener('submit', (e) => { // fire a function on form submi
     addDoc(colRef, {
         title: addBookForm.title.value,
         author: addBookForm.author.value, 
+        createdAt: serverTimestamp()
     })
     .then(() => { // async, clear the form once the user submits (don't refresh the page tho)
         addBookForm.reset()
@@ -78,4 +91,51 @@ deleteBookForm.addEventListener('submit', (e) => { // fire a function on form su
             deleteBookForm.reset()
         })
 
+})
+
+// get a single document
+const docRef = doc(db, 'books', 'hwwzGB8xTD5n954aX3gQ')
+
+getDoc(docRef)
+    .then((doc) => {
+        console.log(doc.data(), doc.id)
+    })
+
+onSnapshot(docRef, (doc) => {
+    console.log(doc.data(), doc.id)
+})
+
+// updating a document
+const updateForm = document.querySelector('.update')
+updateForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'books', updateForm.id.value)
+
+    updateDoc(docRef, {
+        title: 'updated title'
+
+    })
+    .then(()=>{
+        updateForm.reset
+    })
+
+})
+
+// signing users up
+const signupForm = document.querySelector('.signup')
+signupForm.addEventListener('submit',(e)=> {
+    e.preventDefault()
+
+    const email = signupForm.email.value
+    const password = signupForm.password.value
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((cred)=> {
+            console.log('user created:', cred.user)
+            signupForm.reset()
+        })
+        .catch((err)=> {
+            console.log(err.message)
+        })
 })
